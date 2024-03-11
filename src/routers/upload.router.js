27689763174d302d06a20cfca992a1ff84e6bbc4
henry nnamani <nnamani.henry@sevenup.org>
2024-03-11 13:50:21 +0100

    updated

--- src/routers/upload.router.js
+++ src/routers/upload.router.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import admin from '../middleware/admin.mid.js';
 import multer from 'multer';
 import handler from 'express-async-handler';
-import { BAD_REQUEST } from '../constants/httpStatus.js';
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from '../constants/httpStatus.js';
 import { configCloudinary } from '../config/cloudinary.config.js';
 
 const router = Router();
@@ -11,16 +11,21 @@ const upload = multer();
 router.post(
   '/',
   admin,
-  upload.single('image'),
+  upload.single('image'), // Make sure 'image' matches the name of the file input in your form
   handler(async (req, res) => {
-    const file = req.file;
-    if (!file) {
-      res.status(BAD_REQUEST).send();
-      return;
-    }
+    try {
+      const file = req.file;
+      if (!file) {
+        res.status(BAD_REQUEST).send('No file uploaded');
+        return;
+      }
 
-    const imageUrl = await uploadImageToCloudinary(req.file?.buffer);
-    res.send({ imageUrl });
+      const imageUrl = await uploadImageToCloudinary(file.buffer);
+      res.status(200).send({ imageUrl });
+    } catch (error) {
+      console.error('Error uploading image:', error);
+      res.status(INTERNAL_SERVER_ERROR).send('Internal Server Error');
+    }
   })
 );
 
@@ -28,15 +33,19 @@ const uploadImageToCloudinary = imageBuffer => {
   const cloudinary = configCloudinary();
 
   return new Promise((resolve, reject) => {
-    if (!imageBuffer) reject(null);
+    if (!imageBuffer) {
+      reject('No image data provided');
+      return;
+    }
 
-    cloudinary.uploader
-      .upload_stream((error, result) => {
-        if (error || !result) reject(error);
-        else resolve(result.url);
-      })
-      .end(imageBuffer);
+    cloudinary.uploader.upload_stream((error, result) => {
+      if (error || !result) {
+        reject(error || 'Failed to upload image');
+      } else {
+        resolve(result.url);
+      }
+    }).end(imageBuffer);
   });
 };
-
+ 
 export default router;
